c3952353f4dd2bbe9aadfc77ee86e549a5d30096
Melkiy285 <mixaruno@gmail.com>
2024-10-28 12:02:16 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,5 @@
+completed_homework=12
+hours_spent=1.5
+curs_name='Python'
+average_execution_time=(hours_spent/completed_homework)
+print(curs_name,completed_homework,hours_spent,average_execution_time)
